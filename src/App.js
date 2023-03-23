d3a36704b6406e6c93c0b8dc64564a1dc516662f
ismailcanaydin <ismailcanaydin@gmail.com>
2023-03-23 05:05:51 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import logo from './pictures/question.png';
+import { country } from './country';
 
 function App() {
+  console.log(country.name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <ul className='cards'>
+        <li className='card'>
+          <div className='view front-view'>
+            <span className='material-icons' >
+              <img src={logo} />
+            </span>
+          </div>
+          <div className='back-view'>
+            <img src={'./pictures/argentina.png'} alt='card-image' />
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ src/country.js
@@ -0,0 +1,5 @@
+export const country = [{
+    id: 1,
+    name:'turkey',
+    img: './pictures/turkey.png'
+}]
